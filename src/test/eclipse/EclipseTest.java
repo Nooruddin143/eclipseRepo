0a8f2da842118652cfec035f5012604be2d8f9d5
nooruddin <nooruddin.mohammadi@tekschool.us>
2023-02-26 13:30:05 -0800

    this is the second commit from eclipse

---
+++ src/test/eclipse/EclipseTest.java
@@ -0,0 +1,11 @@
+package test.eclipse;
+
+public class EclipseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2" + 2+ "2" );
+
+	}
+
+}
